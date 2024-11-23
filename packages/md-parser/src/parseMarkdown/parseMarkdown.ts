74003c918d521815df2b7c8345430627f9f84976
Ruslan Sedziukh <ruslan.sedziukh@gmail.com>
2024-11-23 20:31:09 +0200

    Parse file and return lines as it is

--- packages/md-parser/src/parseMarkdown/parseMarkdown.ts
+++ packages/md-parser/src/parseMarkdown/parseMarkdown.ts
@@ -1,3 +1,13 @@
-export const parseMarkdown = () => {
-  return 'parse markdown'
+import fs from 'fs'
+
+export const parseMarkdown = (documentPath: string) => {
+  try {
+    const data = fs.readFileSync(documentPath, 'utf8')
+
+    const lines = data.split('\n')
+
+    return lines
+  } catch (err) {
+    throw err
+  }
 }
--- packages/playground/src/app/test/page.tsx
+++ packages/playground/src/app/test/page.tsx
@@ -2,7 +2,7 @@ import { renderMarkdown } from 'md-render'
 import { parseMarkdown } from 'md-parser'
 
 const Test = () => {
-  console.log('markdown:', parseMarkdown())
+  console.log('markdown:', parseMarkdown('md-docs/test.md'))
 
   return <div>{renderMarkdown()}</div>
 }
