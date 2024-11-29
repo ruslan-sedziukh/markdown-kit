240ac2084bb857dc6a33273f96503a7e7549f3e4
Ruslan Sedziukh <ruslan.sedziukh@gmail.com>
2024-11-29 17:56:40 +0200

    Move tests to correct place

---
+++ packages/md-parser/src/parseHeading/parseMarkdownString.test.ts
@@ -0,0 +1,22 @@
+import { parseHeading } from '.'
+
+describe('parseHeading', () => {
+  describe('heading of different levels', () => {
+    it('parses correctly', () => {
+      const heading = 'Heading'
+
+      expect(parseHeading(`# ${heading}`)).toEqual({
+        type: 'heading-1',
+        content: [`${heading}`],
+      })
+      expect(parseHeading(`## ${heading}`)).toEqual({
+        type: 'heading-2',
+        content: [`${heading}`],
+      })
+      expect(parseHeading(`### ${heading}`)).toEqual({
+        type: 'heading-3',
+        content: [`${heading}`],
+      })
+    })
+  })
+})
