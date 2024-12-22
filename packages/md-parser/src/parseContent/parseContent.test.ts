e621727510c8f30db9d2d7afdde93c4fd081336d
Ruslan Sedziukh <ruslan.sedziukh@gmail.com>
2024-12-22 17:06:51 +0200

    Refactor tests

--- packages/md-parser/src/parseContent/parseContent.test.ts
+++ packages/md-parser/src/parseContent/parseContent.test.ts
@@ -2,63 +2,47 @@ import { parseContent } from '.'
 
 describe('parseContent', () => {
   describe('emphasized content', () => {
-    it('parsed correctly in case simple text', () => {
-      const content = 'Heading **one** of two'
-
-      expect(parseContent(content)).toEqual([
-        'Heading ',
-        {
-          type: 'bold',
-          content: ['one'],
-        },
-        ' of two',
-      ])
-    })
-
-    it('returns correct content when only open symbols are without pair', () => {
-      const content = 'Heading **one'
-
-      expect(parseContent(content)).toEqual(['Heading **one'])
-    })
-
-    it('returns correct emphasized content inside another emphasized content', () => {
-      const content = 'Heading ***o*ne**'
-
-      expect(parseContent(content)).toEqual([
-        'Heading ',
-        {
-          type: 'bold',
-          content: [
-            {
-              type: 'italic',
-              content: ['o'],
-            },
-            'ne',
-          ],
-        },
-      ])
-    })
-
-    it('returns correct emphasized content inside another emphasized content', () => {
-      const content = '*H*eading ***o*ne**'
-
-      expect(parseContent(content)).toEqual([
-        {
-          type: 'italic',
-          content: ['H'],
-        },
-        'eading ',
-        {
-          type: 'bold',
-          content: [
-            {
-              type: 'italic',
-              content: ['o'],
-            },
-            'ne',
-          ],
-        },
-      ])
+    it.each([
+      {
+        text: 'is parsed correctly in case simple text',
+        content: 'Heading **one** of two',
+        expected: [
+          'Heading ',
+          {
+            type: 'bold',
+            content: ['one'],
+          },
+          ' of two',
+        ],
+      },
+      {
+        text: 'is not parsed when there are only opened symbols',
+        content: 'Heading **one',
+        expected: ['Heading **one'],
+      },
+      {
+        text: 'is parsed inside another emphasized content',
+        content: '*H*eading ***o*ne**',
+        expected: [
+          {
+            type: 'italic',
+            content: ['H'],
+          },
+          'eading ',
+          {
+            type: 'bold',
+            content: [
+              {
+                type: 'italic',
+                content: ['o'],
+              },
+              'ne',
+            ],
+          },
+        ],
+      },
+    ])('$text', ({ content, expected }) => {
+      expect(parseContent(content)).toEqual(expected)
     })
   })
 })
