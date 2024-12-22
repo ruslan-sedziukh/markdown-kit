ce20d6a36e0b8dcb8d1de5153b67677d29cf6f65
Ruslan Sedziukh <ruslan.sedziukh@gmail.com>
2024-12-22 16:56:02 +0200

    WIP

--- packages/md-parser/src/parseContent/parseContent.test.ts
+++ packages/md-parser/src/parseContent/parseContent.test.ts
@@ -1,70 +1,64 @@
 import { parseContent } from '.'
 
 describe('parseContent', () => {
-  it('return correct emphasized content', () => {
-    const content = 'Heading **one** of two'
+  describe('emphasized content', () => {
+    it('parsed correctly in case simple text', () => {
+      const content = 'Heading **one** of two'
 
-    const parsed = parseContent(content)
+      expect(parseContent(content)).toEqual([
+        'Heading ',
+        {
+          type: 'bold',
+          content: ['one'],
+        },
+        ' of two',
+      ])
+    })
 
-    expect(parseContent(content)).toEqual([
-      'Heading ',
-      {
-        type: 'bold',
-        content: ['one'],
-      },
-      ' of two',
-    ])
-  })
+    it('returns correct content when only open symbols are without pair', () => {
+      const content = 'Heading **one'
 
-  it('return correct content when only open symbols are without pair', () => {
-    const content = 'Heading **one'
+      expect(parseContent(content)).toEqual(['Heading **one'])
+    })
 
-    const parsed = parseContent(content)
+    it('returns correct emphasized content inside another emphasized content', () => {
+      const content = 'Heading ***o*ne**'
 
-    expect(parseContent(content)).toEqual(['Heading **one'])
-  })
+      expect(parseContent(content)).toEqual([
+        'Heading ',
+        {
+          type: 'bold',
+          content: [
+            {
+              type: 'italic',
+              content: ['o'],
+            },
+            'ne',
+          ],
+        },
+      ])
+    })
 
-  it('return correct emphasized content inside another emphasized content', () => {
-    const content = 'Heading ***o*ne**'
+    it('returns correct emphasized content inside another emphasized content', () => {
+      const content = '*H*eading ***o*ne**'
 
-    const parsed = parseContent(content)
-
-    expect(parseContent(content)).toEqual([
-      'Heading ',
-      {
-        type: 'bold',
-        content: [
-          {
-            type: 'italic',
-            content: ['o'],
-          },
-          'ne',
-        ],
-      },
-    ])
-  })
-
-  it('!!return correct emphasized content inside another emphasized content', () => {
-    const content = '*H*eading ***o*ne**'
-
-    const parsed = parseContent(content)
-
-    expect(parseContent(content)).toEqual([
-      {
-        type: 'italic',
-        content: ['H'],
-      },
-      'eading ',
-      {
-        type: 'bold',
-        content: [
-          {
-            type: 'italic',
-            content: ['o'],
-          },
-          'ne',
-        ],
-      },
-    ])
+      expect(parseContent(content)).toEqual([
+        {
+          type: 'italic',
+          content: ['H'],
+        },
+        'eading ',
+        {
+          type: 'bold',
+          content: [
+            {
+              type: 'italic',
+              content: ['o'],
+            },
+            'ne',
+          ],
+        },
+      ])
+    })
   })
 })
