463f2dacb741a9b1abdbece3a26a127fe65f0c2f
Ruslan Sedziukh <ruslan.sedziukh@gmail.com>
2025-02-26 16:11:52 +0200

    Configure Rollup to build package with css without typescript but with issue with index.css import in index.d.ts

--- packages/md-render/rollup.config.js
+++ packages/md-render/rollup.config.js
@@ -1,27 +1,40 @@
-// Contents of the file /rollup.config.js
-import typescript from '@rollup/plugin-typescript'
-import dts from "rollup-plugin-dts"
-import localResolve from 'rollup-plugin-local-resolve'
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
 
 const config = [
   {
-    input: 'dist/index.js',
+    input: 'index.ts', // Input is now your TypeScript source
     output: {
       file: 'dist/md-render.js',
       format: 'es',
       sourcemap: true,
     },
-    external: ['@ruslan-sedziukh/md-types'],
-    plugins: [typescript(), localResolve()]
+    external: ['@ruslan-sedziukh/md-types', 'react'], //Adjust this if needed.
+    plugins: [
+      nodeResolve(),
+      typescript(), // Rollup handles TypeScript compilation
+      postcss({
+        extract: true,
+        modules: true, // Enable CSS modules
+        minimize: true,
+        plugins: [],
+      }),
+      copy({
+        targets: [{ src: 'src/index.css', dest: 'dist' }],
+      }),
+    ],
   },
   {
     input: 'dist/index.d.ts',
     output: {
       file: 'dist/md-render.d.ts',
-      format: 'es'
+      format: 'es',
     },
-    plugins: [dts()]
-  }
-]
+    plugins: [dts()],
+  },
+];
 
-export default config
+export default config;
